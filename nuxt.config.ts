7c94383d2cf80e89302992dcec058e949642aba2
TOGO-ASAI <asaitogo@gmail.com>
2024-07-31 23:16:13 +0900

    nuxt.config.ts format fix.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: true,
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ["@nuxtjs/tailwindcss"],
   runtimeConfig: {
     public: {
       API_ENDPOINT_BASE_URL: process.env.API_ENDPOINT_BASE_URL,
     },
   },
-})
+});
