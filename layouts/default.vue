22e2a4651103f2532dd8901e04f8767e0f738b5b
TOGO-ASAI <asaitogo@gmail.com>
2024-08-02 00:18:48 +0900

    Missing files for last commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="min-h-screen bg-gradient-to-t from-black to-grey-dark">
+  <div class="relative min-h-screen bg-gradient-to-t from-black to-grey-dark">
     <slot />
+    <img
+      class="absolute left-4 top-4 h-10"
+      src="../assets/images/insight-crow_landscape.svg"
+      alt=""
+    />
   </div>
 </template>
 <script setup lang="ts"></script>
